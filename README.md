0d5212937bb32bd054e32c03bef6272374ae0e06
lariam <lariam6@gmail.com>
2022-03-14 23:36:57 +0900

    Add sample.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # trial-github-actions
 Try GitHub Actions
+
+
+
+## About pytest
+*  pytestは、テスト実行時に指定したディレクトリ以下のテストを探索し実行する。(ディレクトリを指定しない場合は、カレントディレクトリ以下のテストを探索)
+* test_*/*_testなどの名前で定義されているモジュールが探索の対象。
+* test_で始まる関数。
+* Testで始まるクラスのtest_で始まるメソッド。
---
+++ test_myapp.py
@@ -0,0 +1,20 @@
+from myapp import add, sub
+
+
+def test_add():
+    assert add(1, 2) == 3
+
+
+def test_sub():
+    assert sub(3, 1) == 2
+
+
+class TestCase:
+    def test_true(self):
+        assert True
+
+    def test_add_zero(self):
+        assert add(1, 0) == 1
+
+    def test_sub_zero(self):
+        assert sub(1, 0) == 1
